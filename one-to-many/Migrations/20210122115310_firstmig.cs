5fcdf295a165c2a6f7ca6d28512725c0e3f0a529
afaqch1 <afaq.saeed@techverx.com>
2021-01-25 13:42:57 +0500

    Rawsql

---
+++ one-to-many/Migrations/20210122115310_firstmig.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace one_to_many.Migrations
+{
+    public partial class firstmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    gradeid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    grade_name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.gradeid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    studentid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    student_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    student_age = table.Column<int>(type: "int", nullable: false),
+                    student_phone = table.Column<long>(type: "bigint", nullable: false),
+                    gradeid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.studentid);
+                    table.ForeignKey(
+                        name: "FK_Students_Grades_gradeid",
+                        column: x => x.gradeid,
+                        principalTable: "Grades",
+                        principalColumn: "gradeid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_gradeid",
+                table: "Students",
+                column: "gradeid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Grades");
+        }
+    }
+}
---
+++ one-to-many/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace one_to_many
+{
+    public class Student
+    {
+        public int studentid { get; set; }
+        public string student_name { get; set; }
+        public int student_age { get; set; }
+        public uint student_phone { get; set; }
+
+        public int gradeid { get; set; }
+        public Grade grade { get; set; }
+    }
+
+    public class Grade
+    {
+        public int gradeid { get; set; }
+        public string grade_name { get; set; }
+    }
+
+    public class ClassRoom : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Server=.;Database=Db2;Trusted_Connection=True;");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context=new ClassRoom())
+            {
+                var std = new Student()
+               {
+                    student_name = "Afaq",
+                    student_age=23,
+                    student_phone=03204450987,
+                    gradeid=1
+                    
+                };
+                var std1 = new Student()
+                {
+                    student_name = "Yasim",
+                    student_age = 24,
+                    student_phone = 0062786328,
+                    gradeid = 4
+                };
+                var std2 = new Student()
+                {
+                    student_name = "Wamik",
+                    student_age = 22,
+                    student_phone = 0333350987,
+                    gradeid = 5
+                };
+                var std3 = new Student()
+                {
+                    student_name = "Umair",
+                    student_age = 99,
+                    student_phone = 03204450987,
+                    gradeid = 2
+                };
+               
+                var grad = new Grade()
+                {
+                    
+                    grade_name="A"
+                };
+                var grad1 = new Grade()
+                {
+                    grade_name = "B"
+                };
+                var grad2 = new Grade()
+                {
+                    grade_name = "C"
+                };
+                var grad3 = new Grade()
+                {
+                    grade_name = "D"
+                };
+                var grad4= new Grade()
+                {
+                    grade_name = "E"
+                };
+                var grad5 = new Grade()
+                {
+                    grade_name = "F"
+                };
+
+                /*context.Grades.Add(grad1);
+                context.Grades.Add(grad2);
+                context.Grades.Add(grad3);
+                context.Grades.Add(grad4);
+                context.Grades.Add(grad5);*/
+
+
+                /*  context.Students.Add(std);
+                  context.Students.Add(std1);
+                  context.Students.Add(std2);
+                  context.Students.Add(std3);*/
+
+
+
+                context.SaveChanges();
+
+
+                var result = from s in context.Students
+                             join g in context.Grades on s.gradeid equals g.gradeid
+                             select new
+                             {
+                                 std_name = s.student_name,
+                                 std_grade = g.grade_name
+                             };
+                foreach (var res in result)
+                    Console.WriteLine("Name:{0}\t\tGrade:{1}", res.std_name, res.std_grade);
+
+                Console.WriteLine("=====================RawSql==============================");
+
+                var rawsql = context.Students.FromSqlRaw("Select * from Students where gradeid<3").ToList();
+                foreach (var r in rawsql)
+                    Console.WriteLine("Name:" + r.student_name + "\tAge:" + r.student_age + "\tGrade:" + r.gradeid);
+                
+            }
+
+        }
+    }
+}
